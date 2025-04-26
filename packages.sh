467ccfaaec7f3239bba9cf21a1d2a8691e25c8f9
abdalrahmanshaban0 <abdalrahmanshaban52@gmail.com>
2025-04-26 13:40:32 +0300

    Adding a premature script for installation

---
+++ packages.sh
@@ -0,0 +1,20 @@
+# !/bin/bash
+
+echo "Installing yay.."
+mkdir .src
+cd .src
+git clone https://aur.archlinux.org/yay.git
+cd yay-git
+makepkg -si
+
+cd .src
+echo "Installing Hyprland.."
+
+yay -S ninja gcc cmake meson libxcb xcb-proto xcb-util xcb-util-keysyms libxfixes libx11 libxcomposite libxrender libxcursor pixman wayland-protocols cairo pango libxkbcommon xcb-util-wm xorg-xwayland libinput libliftoff libdisplay-info cpio tomlplusplus hyprlang-git hyprcursor-git hyprwayland-scanner-git xcb-util-errors hyprutils-git glaze hyprgraphics-git aquamarine-git re2 hyprland-qtutils
+
+git clone --recursive https://github.com/hyprwm/hyprland
+cd Hyprland
+make all && sudo make install
+
+echo "Installing packages.."
+sudo pacman -S uwsm neovim gvim noto-fonts noto-fonts-emoji ttf-liberation ttf-jetbrains-mono ttf-hack-nerd ttf-nerd-fonts-symbols swww waypaper network-manager-applet bluez bluez-utils bluez-obex blueman sudo pacman -S thunar gvfs gvfs-mtp thunar-volman ffmpegthumbnailer tumbler man-db lsd bzip2 gzip p7zip unrar zip unzip alacritty mako jq wl-clipboard wl-clip-persist clipse-bin pipewire pipewire-pulse pipewire-alsa wireplumber pavucontrol hyprpolkitagent grim slurp google-chrome firefox evince nwg-look papirus-icon-theme mpv playerctl wlogout hyprlock obsidian lazygit btop swappy cups cups-pdf ghostscript gsfonts gutenprint system-config-printer gimp wine wine-mono wine-gecko winetricks quran-companion obs-studio xdg-desktop-portal-hyprland
