986cd142f6dd833d26bdaf73c35c11a93eaab084
abdalrahmanshaban0 <abdalrahmanshaban52@gmail.com>
2025-08-04 20:24:23 +0300

    Adding action to screenshots: Open image path in thunar file manager

--- config/hypr/scripts/screenshot.sh
+++ config/hypr/scripts/screenshot.sh
@@ -1,1 +1,14 @@
-mkdir -p ~/Pictures/Screenshots && grim -g "$(slurp)" - | wl-copy && wl-paste > ~/Pictures/Screenshots/Screenshot-$(date +%F_%T).png && notify-send "Screenshot saved in ~/Pictures/Screenshots" -t 3000
+#!/bin/bash
+
+# Create screenshot directory
+mkdir -p ~/Pictures/Screenshots
+
+# Capture screenshot
+FILE=~/Pictures/Screenshots/Screenshot-$(date +%F_%T).png
+grim -g "$(slurp)" - | wl-copy && wl-paste > "$FILE"
+
+# Send a clickable notification
+notify-send -t 3000 \
+    --action="Open image path" \
+    "Screenshot saved" "$FILE" \
+    && thunar "$FILE"
