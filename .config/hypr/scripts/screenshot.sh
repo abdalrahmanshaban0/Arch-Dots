2485417bb80529b335fc2ced10ec6165cc2aa457
abdalrahmanshaban0 <abdalrahmanshaban52@gmail.com>
2025-08-25 13:57:11 +0300

    Screenshots: Edit the script to use grimblast

--- .config/hypr/scripts/screenshot.sh
+++ .config/hypr/scripts/screenshot.sh
@@ -5,10 +5,5 @@ mkdir -p ~/Pictures/Screenshots
 
 # Capture screenshot
 FILE=~/Pictures/Screenshots/Screenshot-$(date +%F_%T).png
-grim -g "$(slurp)" - | wl-copy && wl-paste > "$FILE"
 
-# Send a clickable notification
-notify-send -t 3000 \
-    --action="Open image path" \
-    "Screenshot saved" "$FILE" \
-    && thunar "$FILE"
+grimblast --notify copysave area $FILE
