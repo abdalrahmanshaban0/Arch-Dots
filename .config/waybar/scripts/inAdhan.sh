b282aa3d8debf0783bca890bb31c46357add8805
abdalrahmanshaban0 <abdalrahmanshaban52@gmail.com>
2025-09-03 20:55:19 +0300

    Waybar: Moving media and inAdhan script update

--- .config/waybar/scripts/inAdhan.sh
+++ .config/waybar/scripts/inAdhan.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # play adhan audio
-AUDIO_FILE="$HOME/.config/waybar/media/adhan-mansour-al-zahrani.mp3"
+AUDIO_FILE="$HOME/.local/share/IslamicPrayerTimings/adhan-mansour-al-zahrani.mp3"
 mpv --no-video --loop=no --really-quiet "$AUDIO_FILE" &
 sleep 10
 # lock screen (for hyprland)
