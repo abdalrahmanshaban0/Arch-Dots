98a1a70f0be1c0c685eb1079466de7320b6e0d05
abdalrahmanshaban0 <abdalrahmanshaban52@gmail.com>
2025-09-22 23:25:42 +0300

    Adding fish shell config

---
+++ .config/fish/config.fish
@@ -0,0 +1,5 @@
+source ~/.config/fish/themes/tokyonight_night.fish
+
+alias ls="eza -l --color --icons --git --group-directories-first"
+alias la="eza -la --color --icons --git --group-directories-first"
+alias grep="grep --color=auto"
